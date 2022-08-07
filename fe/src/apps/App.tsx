7773fa45c466023c7f0ef4337409fe20652c46e5
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-08-07 21:40:06 +0900

    grid design

--- fe/src/apps/App.tsx
+++ fe/src/apps/App.tsx
@@ -1,5 +1,5 @@
 import {
-  CssBaseline, Box, Container, Button, Typography,
+  Grid, Paper, CssBaseline, Box, Container, Button, Typography,
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { useState } from 'react';
@@ -7,6 +7,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { BoldSpan, BoldDiv } from '../components/BoldBox';
 import StyledAppBar from '../components/StyledAppBar';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: 'yellow',
+  ...theme.typography.body2,
+  padding: theme.spacing(3),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const CustomButton = styled(Button)({
   backgroundColor: 'royalblue',
@@ -28,6 +36,22 @@ const ExampleApp = () => {
 };
 const BoxExample = () => (
   <>
+    <Typography variant="h4">Grid</Typography>
+    <Grid container columnSpacing={10} rowSpacing={2}>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={8}>
+        <Item>xs=8</Item>
+      </Grid>
+    </Grid>
+    <Box m={5} />
     <Typography variant="h4">BOLD</Typography>
     <Typography variant="h6">
       When you want to use bold span, use
@@ -66,6 +90,18 @@ const BoxExample = () => (
     >
       <div>2. Box</div>
       <div>use alignItems and justifyContent with Box</div>
+    </Box>
+    <Box m={5} />
+    <Typography variant="h4">m (margin) and p (padding)</Typography>
+    <Typography variant="h6">
+      unit is `theme.space`.
+      theme.space(1) is 4px by default.
+    </Typography>
+    <Box textAlign="center">
+      <Box m={10} p={0} bgcolor="#ef5350">m=10 p=0 </Box>
+      <Box m={0} p={0} bgcolor="#ab47bc">m=0 p=0</Box>
+      <Box m={0} p={10} bgcolor="#5c6bc0">m=0 p=10</Box>
+      <Box m={10} p={10} bgcolor="#29b6f6">m=10 p=10</Box>
     </Box>
   </>
 );
