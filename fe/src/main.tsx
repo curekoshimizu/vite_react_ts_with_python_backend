4b7e5ecf176b3cd11736fe95faae2a98ed0f6307
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-08-07 15:59:36 +0900

    add PWA settings in vite.config.ts

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 
 import App from './App';
 
@@ -8,3 +9,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>,
 );
+
+registerSW();
