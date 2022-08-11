2af737fed8d6204af63d2c2f6d24abe9ec8ea60e
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-08-11 11:42:14 +0900

    openapi generator

---
+++ generate_client_code.bash
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+
+pushd be > /dev/null
+poetry run ./scripts/generate_openapi.py
+popd > /dev/null
+
+OPENAPI_YAML="openapi.yaml"
+
+if [ ! -e "${OPENAPI_YAML}" ]; then
+    echo "${OPENAPI_YAML} not found."
+    exit 1
+fi
+if [ ! -e "${PWD}/${OPENAPI_YAML}" ]; then
+    echo "${OPENAPI_YAML} not found in this directory."
+    exit 1
+fi
+
+docker run --rm -v "${PWD}:/local" \
+    openapitools/openapi-generator-cli generate \
+    -i /local/${OPENAPI_YAML} \
+    -g typescript-fetch \
+    -o /local/fe/src/client
