5d66d42f6c7e24669d35a413fdc7a0d7153736d8
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-08-11 17:14:26 +0900

    docs option and static option

--- be/be/app.py
+++ be/be/app.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 from fastapi import APIRouter, FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -17,5 +18,15 @@ async def ping() -> list[Response]:
 
 
 app = FastAPI()
-app.include_router(router, prefix="/api")
-app.mount("/", StaticFiles(directory="../fe/dist/", html=True), name="static")
+
+
+def setup_app(
+    use_docs: bool,
+    static_dir: Optional[str] = None,
+) -> None:
+    if not use_docs:
+        # disable docs routers which has been already registered in constructor
+        app.router.routes = []
+    app.include_router(router, prefix="/api")
+    if static_dir is not None:
+        app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
--- be/scripts/run_server.py
+++ be/scripts/run_server.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
 
+import pathlib
+
 import click
 import uvicorn  # type:ignore
 
-from be.app import app
+from be.app import app, setup_app
 
 
 @click.command()
-@click.option("--port", default=13000, help="port number")
+@click.option("--port", default=8080, help="port number")
 @click.option("--reload", is_flag=True, help="enable hot reload")
-def main(port: int, reload: bool) -> None:
-    # document urls
-    print("[document urls]")
-    print(" * redoc url : ", app.redoc_url)
-    print(" * docs_url : ", app.docs_url)
-    print(" * openapi_url : ", app.openapi_url)
+@click.option("--docs", is_flag=True, help="enable documents")
+@click.option("--static", default="../fe/dist", help="static files built by fe dir")
+def main(port: int, reload: bool, docs: bool, static: str) -> None:
+    setup_app(use_docs=docs, static_dir=static if pathlib.Path(static).exists() else None)
+
+    # # document urls
+    if docs:
+        print("[document urls]")
+        print(" * redoc url : ", app.redoc_url)
+        print(" * docs_url : ", app.docs_url)
+        print(" * openapi_url : ", app.openapi_url)
     uvicorn.run("be.app:app", host="0.0.0.0", port=port, log_level="info", reload=reload)
 
 
