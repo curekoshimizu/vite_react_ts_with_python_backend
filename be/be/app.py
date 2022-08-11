22b63f83e55c98fe30f1bf46f7697b268877f212
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-08-11 15:03:51 +0900

    publish build files by fe

--- be/be/app.py
+++ be/be/app.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
-
 from dataclasses import dataclass
 
 from fastapi import APIRouter, FastAPI
+from fastapi.staticfiles import StaticFiles
 
 router = APIRouter()
 
@@ -19,3 +18,4 @@ async def ping() -> list[Response]:
 
 app = FastAPI()
 app.include_router(router, prefix="/api")
+app.mount("/", StaticFiles(directory="../fe/dist/", html=True), name="static")
