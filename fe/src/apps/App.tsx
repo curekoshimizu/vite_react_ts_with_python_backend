fec533c7076fddcaecc710e95b2b5813107fc80f
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-08-19 21:36:31 +0900

    NotFound page

--- fe/src/apps/App.tsx
+++ fe/src/apps/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StyledAppBar from '../components/StyledAppBar';
 import BoxExample from '../pages/BoxExample';
 import ReduxExampleApp from '../pages/ReduxExample';
+import NotFound from "../pages/NotFound";
 
 const links = [
   { label: 'box example', path: '/' },
@@ -18,6 +19,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<BoxExample />} />
         <Route path="/redux_example" element={<ReduxExampleApp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Container>
   </BrowserRouter>
--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
       open: true,
       proxy: {
-          "^/api/.*": "http://localhost:13000",
+          "^/api/.*": "http://localhost:8080",
       },
   },
   plugins: [
