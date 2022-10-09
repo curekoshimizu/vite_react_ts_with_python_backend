f2ea36f4e5d62bb9ef86b7b700c760174ea8e566
curekoshimizu <45332899+curekoshimizu@users.noreply.github.com>
2022-10-09 21:37:15 +0900

    linksLeft and linksRight

--- fe/src/apps/App.tsx
+++ fe/src/apps/App.tsx
@@ -1,25 +1,55 @@
 import { CssBaseline, Container } from '@mui/material';
+import { ReactNode } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import StyledAppBar from '../components/StyledAppBar';
+import { StyledAppBar, ILink } from '../components/StyledAppBar';
 import BoxExample from '../pages/BoxExample';
 import ReduxExampleApp from '../pages/ReduxExample';
 import NotFound from "../pages/NotFound";
 
-const links = [
-  { label: 'box example', path: '/' },
-  { label: 'redux example', path: '/redux_example' },
+const defaultComponent: ReactNode = <NotFound />;
+
+interface LinkWithComponent extends ILink {
+  element: ReactNode
+}
+
+const linksLeft : LinkWithComponent[] = [
+  {
+    label: 'box example',
+    path: '/',
+    visible: true,
+    element: <BoxExample />,
+  },
+  {
+    label: 'redux example',
+    path: '/redux_example',
+    visible: true,
+    element: <ReduxExampleApp />,
+  },
+];
+const linksRight: LinkWithComponent[] = [
 ];
 
 const App = () => (
   <BrowserRouter>
     <CssBaseline />
     <Container maxWidth="lg" fixed>
-      <StyledAppBar links={links} />
+      <StyledAppBar
+        linksLeft={linksLeft}
+        linksRight={linksRight}
+      />
       <Routes>
-        <Route path="/" element={<BoxExample />} />
-        <Route path="/redux_example" element={<ReduxExampleApp />} />
-        <Route path="*" element={<NotFound />} />
+        {
+            linksLeft.map((e) => (
+              <Route key={e.path} path={e.path} element={e.element} />
+            ))
+        }
+        {
+            linksRight.map((e) => (
+              <Route key={e.path} path={e.path} element={e.element} />
+            ))
+        }
+        <Route path="*" element={defaultComponent} />
       </Routes>
     </Container>
   </BrowserRouter>
--- fe/src/components/StyledAppBar.tsx
+++ fe/src/components/StyledAppBar.tsx
@@ -1,5 +1,5 @@
 import {
-  AppBar, Toolbar, Button,
+  AppBar, Toolbar, Button, Box,
 } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { NavLink, useLocation } from 'react-router-dom';
@@ -28,29 +28,54 @@ const LinkedButton = ({ title, path }: LinkedButtonProp) => {
   );
 };
 
-interface ILink {
+export interface ILink {
   path: string;
   label: string;
+  visible: boolean;
 }
 
-interface StyledAppBarProp {
-  links: ILink[];
+export interface StyledAppBarProp {
+  linksLeft: ILink[];
+  linksRight: ILink[];
 }
 
 const StyledAppBarBase = styled(AppBar)({
   marginBottom: 20,
 });
-const StyledAppBar = ({ links } : StyledAppBarProp) => {
+export const StyledAppBar = ({
+  linksLeft,
+  linksRight,
+} : StyledAppBarProp) => {
+  const currentLocation = useLocation();
+
   return (
     <StyledAppBarBase position="static">
       <Toolbar>
-        {links.map((link) => (
-          <LinkedButton
-            key={link.label}
-            title={link.label}
-            path={link.path}
-          />
-        ))}
+        {linksLeft.map((link) => {
+          if (!link.visible) {
+            return <></>;
+          }
+          return (
+            <LinkedButton
+              key={link.label}
+              title={link.label}
+              path={link.path}
+            />
+          );
+        })}
+        <Box sx={{ flexGrow: 1 }} />
+        {linksRight.map((link) => {
+          if (!link.visible) {
+            return <></>;
+          }
+          return (
+            <LinkedButton
+              key={link.label}
+              title={link.label}
+              path={link.path}
+            />
+          );
+        })}
       </Toolbar>
     </StyledAppBarBase>
   );
